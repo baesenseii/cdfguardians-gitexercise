c3e816bdffab062ca2edfcd6d56acdd60c2d76d7
xxbaemaxx <xxbaemaxx@gmail.com>
2020-03-30 01:02:11 +0800

    First commit into program.py

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-# cdfguardians-gitexercise
-Git Coding Exercise
+# CDF Guardians (Git Coding Exercise)
+
+## Objectives
+- To use this repository as an introduction to GitHub
+- To be used for the Docker Basics 101 course later
+
+## Git Coding Challenge
+
+### Problem 1
+Given that a user submits a string of 16 alphanumeric characters:
+- convert each alphanumeric character to an ASCII hex value
+- join all the alphanumeric characters all together into one.
+
+For example: User enters "a,b,c", output should be 616263
+
+### Problem 2
---
+++ program.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python3
+
+def problem1():
+  print("Type your code here")
+
+def problem2():
+  print("Type your stuff here")
+
+def problem3():
+  print("Type your code here")
+
+def problem4():
+  print("Type your stuff here")
+
+def problem5():
+  print("Type your code here")
+
+def problem6():
+  print("Type your stuff here")
+
+def main():
+    banner = """
+ ____  ____  _     _     ____  ____    ____  ____  ____  _  _      _____
+/   _\/  _ \/ \   / \   /  _ \/  __\  /   _\/  _ \/  _ \/ \/ \  /|/  __/
+|  /  | / \|| |   | |   | / \|| | //  |  /  | / \|| | \|| || |\ ||| |  _
+|  \__| \_/|| |_/\| |_/\| |-||| |_\\  |  \__| \_/|| |_/|| || | \||| |_//
+\____/\____/\____/\____/\_/ \|\____/  \____/\____/\____/\_/\_/  \|\____\
+  """
+    print(banner)
+    print ("""
+  Main Menu Options:
+    1. Problem 1
+    2. Problem 2
+    3. Problem 3
+    4. Problem 4
+    5. Problem 5
+    6. Problem 6
+ """)
+    choice = input("Enter your choice here: ")
+
+    if int(choice) == 1:
+        problem1()
+    elif int(choice) == 2:
+        problem2()
+    elif int(choice) == 3:
+        problem3()
+    elif int(choice) == 4:
+        problem4()
+    elif int(choice) == 5:
+        problem5()
+    elif int(choice) == 6:
+        problem6()
+    else:
+        print("Invalid choice. Exiting application")
+
+
+main()
